35a2c27d058cbcef045c2dfe76388a9923995bdd
Francisco-Fortes <fortesfco@gmail.com>
2023-02-20 22:19:02 +0100

    App updated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
-
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import CustomNav from "./components/Nav/CustomNav";
+import LoginComponent from "./components/Login";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <CustomNav></CustomNav>
+      <LoginComponent></LoginComponent>
+    </div>
+  );
 }
 
 export default App;
