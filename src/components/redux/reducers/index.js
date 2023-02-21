5d26a44c2bb75955290611e35165c21c7b92a037
Catriona Ferguson <114424301+cferg97@users.noreply.github.com>
2023-02-21 12:47:30 +0000

    Update index.js

--- src/components/redux/reducers/index.js
+++ src/components/redux/reducers/index.js
@@ -8,15 +8,27 @@ const initialState = {
 };
 //set up the initial state and type (these will be arrays)
 
-
 //declare name of reducer, set the state to be initial state and pass action as an argument
 const mainReducer = (state = initialState, action) => {
   switch (action.type) {
+    case SAVE_CURRENT_USER: {
+      //declare case name
+      return {
+        ...state, //return the rest of the state, pass in the payload as currentUser
+        currentUser: action.payload,
+      };
+    }
 
+    case SAVE_ALL_CONVERSATIONS: {
+      return {
+        ...state,
+        allConversations: action.payload,
+      };
+    }
 
     default:
       return state;
-      //default state needed in case of any errors, it just returns the original state
+    //default state needed in case of any errors, it just returns the original state
   }
 };
 
